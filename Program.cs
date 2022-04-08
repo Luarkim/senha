dc6b4d7d41d09bb85c4d609418be39c01bb6ffc4
luarkim <parkleeh31@gmail.com>
2022-04-08 19:58:26 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿const string SenhaCorreta = "dasvi1409";
+string senha, permissaoAcesso;
+
+Console.Write ("Olá usuário. Por favor Digite sua senha: ");
+senha = Console.ReadLine();
+
+permissaoAcesso = senha == SenhaCorreta ? "permitido" : "negado";
+
+Console.WriteLine($"Acesso {permissaoAcesso} ");
+
+
